59c0bb7559dd6643302c9656e4870981bf209a2a
shadeslasher <deepicman@gmail.com>
2022-01-29 23:15:37 -0500

    WormHoles

--- Assets/Scripts/Wormhole.cs
+++ Assets/Scripts/Wormhole.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 public class Wormhole : MonoBehaviour
 {
 
+    Coroutine releasedCoroutine;
+
     public ParticleSystem warpFX;
     public Wormhole warpLocation;
     public bool canWarp = true;
+    public Vector3 scaleChange;
+    public Vector3 originalScale;
+    void Start() {
+        originalScale = gameObject.transform.localScale;
+        scaleChange = new Vector3((originalScale.x)*-0.00025f, (originalScale.y)*-0.00025f, (originalScale.z)*-0.00025f);
+    }
+
+    void Update() {
+
+        //print(gameObject.name);
+        gameObject.transform.localScale += scaleChange;
+        gameObject.transform.Rotate(0.0f, 0.0f, 0.05f);
+
+        if (gameObject.transform.localScale.y < (originalScale.y * (0.75f)) || gameObject.transform.localScale.y > (originalScale.y * (1.0f)))
+        {
+            scaleChange =- scaleChange;
+        }
+    }
 
     private void OnTriggerEnter(Collider hit) {
         if ((hit.gameObject.layer == 6) && (canWarp == true)) {
